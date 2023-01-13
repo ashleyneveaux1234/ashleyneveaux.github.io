b1008dfc3288066785987a72bd518954d40fccfc
ashleyneveaux1234 <ashleyneveaux@gmail.com>
2023-01-13 15:32:19 +0000

    describe your changes

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,48 @@
+(function (window) {
+  "use strict";
+  window.opspark = window.opspark || {};
+  window.opspark.platform = window.opspark.platform || {};
+
+  let platform = window.opspark.platform;
+
+  /**
+   * init: This function initializes the platforms for the level.
+   *
+   * GOAL: Add as many platforms necessary to make your level challenging.
+   *
+   * Use the createPlatform Function to create platforms for the level.
+   *
+   * createPlatform() takes these arguments:
+   *
+   *      createPlatform(x, y, scaleX, scaleY);
+   *
+   *      x: The x coordineate for the platform.
+   *      y: The y coordineate for the platform.
+   *      scaleX: OPTIONAL The scale factor on the x-axis, this value will
+   *              stretch the platform in width.
+   *      scaleY: OPTIONAL The scale factor on the y-axis, this value will
+   *              stretch the platform in height.
+   */
+  function init(game) {
+    let createPlatform = platform.create;
+
+    ////////////////////////////////////////////////////////////////////////
+    // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+
+    /*
+     * ground : here, we create a floor. Given the width of of the platform
+     * asset, giving it a scaleX and scaleY of 2 will stretch it across the
+     * bottom of the game.
+     */
+    createPlatform(0, game.world.height 47 , 3, 2); // DO NOT DELETE
+
+    // example:
+    createPlatform(300, 250);//top platform
+    createPlatform(220, 660, 0.5);//middle platform
+    createPlatform(400, 660, 0.5);//unnamed platform
+    createPlatform(400, 660, 0.5);//unnamed platform
+    // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+  }
+  platform.init = init;
+})(window);
